59858c1364eb271e35e3051a846346de95cff6b9
Terraeb <mihalyovvalik@gmail.com>
2024-06-05 10:17:22 +0100

    add another files

---
+++ assets/js/ingredients.js
@@ -0,0 +1,226 @@
+const ingredients = [
+    {
+        "id": 1,
+        "name": "Ice",
+        "color": "rgb(230, 230, 230)",
+        "sweetness": 0,
+        "tartness": 0,
+        "bitterness": 0
+    },
+    {
+        "id": 2,
+        "name": "Mint",
+        "color": "rgb(144, 238, 144)",
+        "sweetness": 2,
+        "tartness": 1,
+        "bitterness": 1
+    },
+    {
+        "id": 3,
+        "name": "Sugar",
+        "color": "rgb(255, 255, 204)",
+        "sweetness": 6,
+        "tartness": 0,
+        "bitterness": 0
+    },
+    {
+        "id": 4,
+        "name": "Vodka",
+        "color": "rgb(255, 255, 255)",
+        "sweetness": 0,
+        "tartness": 0,
+        "bitterness": 0
+    },
+    {
+        "id": 5,
+        "name": "White Rum",
+        "color": "rgb(235, 235, 235)",
+        "sweetness": 5,
+        "tartness": 1,
+        "bitterness": 2
+    },
+    {
+        "id": 6,
+        "name": "Gin",
+        "color": "rgb(219, 224, 236)",
+        "sweetness": 0,
+        "tartness": 1,
+        "bitterness": 3
+    },
+    {
+        "id": 7,
+        "name": "Tequila",
+        "color": "rgb(255, 204, 153)",
+        "sweetness": 2,
+        "tartness": 1,
+        "bitterness": 2
+    },
+    {
+        "id": 8,
+        "name": "Lime Juice",
+        "color": "rgb(194, 255, 194)",
+        "sweetness": 1,
+        "tartness": 9,
+        "bitterness": 1
+    },
+    {
+        "id": 9,
+        "name": "Lemon Juice",
+        "color": "rgb(255, 255, 153)",
+        "sweetness": 1,
+        "tartness": 9,
+        "bitterness": 1
+    },
+    {
+        "id": 10,
+        "name": "Orange Juice",
+        "color": "rgb(255, 153, 51)",
+        "sweetness": 8,
+        "tartness": 2,
+        "bitterness": 1
+    },
+    {
+        "id": 11,
+        "name": "Cranberry Juice",
+        "color": "rgb(220, 20, 60)",
+        "sweetness": 6,
+        "tartness": 3,
+        "bitterness": 0
+    },
+    {
+        "id": 12,
+        "name": "Pineapple Juice",
+        "color": "rgb(255, 204, 0)",
+        "sweetness": 7,
+        "tartness": 2,
+        "bitterness": 1
+    },
+    {
+        "id": 13,
+        "name": "Coconut Cream",
+        "color": "rgb(245, 245, 220)",
+        "sweetness": 8,
+        "tartness": 0,
+        "bitterness": 1
+    },
+    {
+        "id": 14,
+        "name": "Cointreau",
+        "color": "rgb(255, 219, 88)",
+        "sweetness": 8,
+        "tartness": 4,
+        "bitterness": 1
+    },
+    {
+        "id": 15,
+        "name": "Kahlúa",
+        "color": "rgb(71, 38, 0)",
+        "sweetness": 6,
+        "tartness": 0,
+        "bitterness": 3
+    },
+    {
+        "id": 16,
+        "name": "Simple Syrup",
+        "color": "rgb(255, 255, 204)",
+        "sweetness": 7,
+        "tartness": 0,
+        "bitterness": 0
+    },
+    {
+        "id": 17,
+        "name": "Grenadine",
+        "color": "rgb(255, 102, 102)",
+        "sweetness": 9,
+        "tartness": 1,
+        "bitterness": 0
+    },
+    {
+        "id": 18,
+        "name": "Triple Sec",
+        "color": "rgb(255, 204, 102)",
+        "sweetness": 7,
+        "tartness": 2,
+        "bitterness": 1
+    },
+    {
+        "id": 19,
+        "name": "Bitters",
+        "color": "rgb(153, 0, 0)",
+        "sweetness": 0,
+        "tartness": 1,
+        "bitterness": 9
+    },
+    {
+        "id": 20,
+        "name": "Salt",
+        "color": "rgb(245, 245, 245)",
+        "sweetness": 0,
+        "tartness": 0,
+        "bitterness": 0
+    },
+    {
+        "id": 21,
+        "name": "Club Soda",
+        "color": "rgb(230, 230, 250)",
+        "sweetness": 0,
+        "tartness": 0,
+        "bitterness": 0
+    },
+    {
+        "id": 22,
+        "name": "Ginger Beer",
+        "color": "rgb(255, 204, 153)",
+        "sweetness": 5,
+        "tartness": 2,
+        "bitterness": 0
+    },
+    {
+        "id": 23,
+        "name": "Cola",
+        "color": "rgb(70, 35, 35)",
+        "sweetness": 8,
+        "tartness": 3,
+        "bitterness": 2
+    },
+    {
+        "id": 24,
+        "name": "Lemonade",
+        "color": "rgb(255, 255, 153)",
+        "sweetness": 9,
+        "tartness": 1,
+        "bitterness": 0
+    }, 
+    {
+        "id": 27,
+        "name": "Coconut Milk",
+        "color": "rgb(218, 165, 32)",
+        "sweetness": 5,
+        "tartness": 0,
+        "bitterness": 0
+    },
+    {
+        "id": 28,
+        "name": "Cream",
+        "color": "rgb(255, 255, 255)",
+        "sweetness": 4,
+        "tartness": 0,
+        "bitterness": 0
+    },
+    {
+        "id": 29,
+        "name": "Egg White",
+        "color": "rgb(255, 255, 255)",
+        "sweetness": 0,
+        "tartness": 0,
+        "bitterness": 0
+    },
+    {
+        "id": 30,
+        "name": "Mango Juice",
+        "color": "rgb(255, 204, 102)",
+        "sweetness": 8,
+        "tartness": 1,
+        "bitterness": 1
+    }
+];
---
+++ assets/js/recipes.js
@@ -0,0 +1,21 @@
+const recipes = [
+    {
+        name: "Mojito",
+        descript: "Mojito is a classic Cuban cocktail made with white rum, fresh mint leaves, lime juice, sugar, and soda water. It's a refreshing and minty drink perfect for hot summer days.",
+        ingredients: [8, 2, 3, 5, 21]
+    }, {
+        name: "Margarita",
+        descript: "Refreshing cocktail made with tequila, lime juice, and triple sec.",
+        ingredients: [8, 7, 18]
+    },
+    {
+        name: "Piña Colada",
+        descript: "A tropical cocktail made with rum, coconut cream, and pineapple juice, often served with a pineapple slice or maraschino cherry.",
+        ingredients: [5, 12, 13]
+    },
+    {
+        name: "Daiquiri",
+        descript: "A simple yet delicious cocktail made with white rum, lime juice, and simple syrup.",
+        ingredients: [5, 9, 16]
+    }
+];
